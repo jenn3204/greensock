c00953a68b69eab5d240761b6ce9374c49ab0c90
jenniferjaque <jenn3204@stud.kea.dk>
2020-03-17 19:58:23 +0100

    exercise 2 complete

--- beachball.js
+++ beachball.js
@@ -11,3 +11,21 @@ function start() {
     gsap.to(".ball", { scale: 0, y: 30, duration: 2 });
   }, 2000);
 }
+
+// other ways
+
+function start1() {
+  gsap.to(".ball", { x: 400, duration: 2 });
+  gsap.to(".ball", { rotate: 200, duration: 2 });
+
+  gsap.to(".ball", { delay: 2, scale: 0, y: 30, duration: 2 });
+}
+
+function start2() {
+  gsap.to(".ball", { x: 400, duration: 2 });
+  gsap.to(".ball", { rotate: 200, duration: 2, onComplete: next });
+}
+
+function next() {
+  gsap.to(".ball", { delay: 2, scale: 0, y: 30, duration: 2 });
+}
--- multiballs.js
+++ multiballs.js
@@ -1,1 +1,8 @@
 "use strict";
+import { gsap } from "gsap";
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  gsap.from(".ball", { y: -400, duration: 2, stagger: 0.2 });
+}
