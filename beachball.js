836703e07d0da72694c96e6621521f412535fde8
jenniferjaque <jenn3204@stud.kea.dk>
2020-03-17 20:25:08 +0100

    exercise 3 complete

--- beachball.js
+++ beachball.js
@@ -3,7 +3,19 @@ import { gsap } from "gsap";
 
 window.addEventListener("DOMContentLoaded", start);
 
+// exercise 3
+
 function start() {
+  let timeline = gsap.timeline({ repeat: 2 });
+
+  timeline.to(".ball", { x: 400, rotate: 200, duration: 2 });
+  timeline.to(".ball", { scale: 0.2, y: 30, duration: 2 });
+  timeline.to(".ball", { y: -700, x: 1100, duration: 2 });
+}
+
+//exercise 1
+
+function start0() {
   gsap.to(".ball", { x: 400, duration: 2 });
   gsap.to(".ball", { rotate: 200, duration: 2 });
 
