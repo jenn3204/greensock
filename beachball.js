cc3eee2e4a7935e7689d7501ea657c663f976447
jenniferjaque <jenn3204@stud.kea.dk>
2020-03-17 17:35:52 +0100

    greensock exercise 1 complete

--- beachball.js
+++ beachball.js
@@ -1,1 +1,13 @@
 "use strict";
+import { gsap } from "gsap";
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  gsap.to(".ball", { x: 400, duration: 2 });
+  gsap.to(".ball", { rotate: 200, duration: 2 });
+
+  setTimeout(function() {
+    gsap.to(".ball", { scale: 0, y: 30, duration: 2 });
+  }, 2000);
+}
